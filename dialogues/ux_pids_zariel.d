d4f3fdaa774f38eb6dfb3a872d884e4830e95682
Matthew Carter <m@ahungry.com>
2022-12-09 16:00:35 -0500

    Working mute dialogue

--- dialogues/ux_pids_zariel.d
+++ dialogues/ux_pids_zariel.d
@@ -4,15 +4,17 @@ BEGIN uxzarj
 // This includes PIDS + NIDS (npc initiated dialogue)
 
 IF ~Global("ux_zariel_is_bantering", "GLOBAL", 1)
-    Global("ux_zariel_banter_id", "GLOBAL", 1)~ zariel_banter1 // THEN BEGIN zariel_banter1
+    Global("ux_zariel_banter_id", "GLOBAL", 0)~ zariel_banter1 // THEN BEGIN zariel_banter1
 SAY ~This is our first banter~
-++ ~Cool!~ EXIT
+++ ~Cool!~ DO ~IncrementGlobal("ux_zariel_banter_id", "GLOBAL", 1)~ EXIT
+++ ~Not now...~ + npc_mute
 END
 
 IF ~Global("ux_zariel_is_bantering", "GLOBAL", 1)
-    Global("ux_zariel_banter_id", "GLOBAL", 2)~ zariel_banter2 // THEN BEGIN zariel_banter1
+    Global("ux_zariel_banter_id", "GLOBAL", 1)~ zariel_banter2 // THEN BEGIN zariel_banter1
 SAY ~This is second first banter~
-++ ~Oooh, neat!~ EXIT
+++ ~Yippie!~ DO ~IncrementGlobal("ux_zariel_banter_id", "GLOBAL", 1)~ EXIT
+++ ~Not now...~ + npc_mute
 END
 
 IF ~IsGabber(Player1)
@@ -37,4 +39,26 @@ END
 IF ~~ quest2
   SAY ~It is my pleasure. In the face of such danger, it is comforting to have companions to rely upon. I will fight to protect both of you, even in the darkest of times.~
   IF ~~ EXIT
+END
+
+IF ~~ npc_mute
+  SAY ~Fine.  How much alone time do you need?~
+  ++ ~Just a few minutes, I'm working on something.~ + npc_mute_short
+  ++ ~I have more important tasks to focus on - give me a few hours.~ + npc_mute_long
+  ++ ~Sorry, that was a mistake, I do have time.~ + npc_mute_undo
+END
+
+IF ~~ npc_mute_short
+  SAY ~Fine.~
+  IF ~~ DO ~RealSetGlobalTimer("ux_zariel_banter_timer", "GLOBAL", 10)~ EXIT
+END
+
+IF ~~ npc_mute_long
+  SAY ~Fine.~
+  IF ~~ DO ~RealSetGlobalTimer("ux_zariel_banter_timer", "GLOBAL", 30)~ EXIT
+END
+
+IF ~~ npc_mute_undo
+  SAY ~Ok, so anyways...~
+  IF ~~ DO ~RealSetGlobalTimer("ux_zariel_banter_timer", "GLOBAL", 1)~ EXIT
 END
